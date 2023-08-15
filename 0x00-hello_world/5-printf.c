be65d115381d43bedbec15e62e8fa577e4a1ba53
nathanwanje <nathanwanje76@gmail.com>
2023-08-15 12:28:47 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Program that prints line using printf function
+ * Return: 0 (Good)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,/n");
+	return (0);
+}
