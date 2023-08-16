94fa5c3d1221c5261c9cd8240341c10f78cb67d1
nathanwanje <nathanwanje76@gmail.com>
2023-08-16 14:51:28 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <unstd.h>
+/**
+ * main - A program printing a line to standard error
+ * Return 1 (Good)
+ */
+int main(void)
+{
+	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
