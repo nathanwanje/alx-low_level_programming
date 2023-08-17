98603d69ed300dd09a397b998d5f186146a57c93
nathanwanje <nathanwanje76@gmail.com>
2023-08-17 12:33:32 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * more_numbers - print more numbers from 0 to 14
+ */
+void more_numbers()
+{
+	int i, j;
+
+	for (i = 1; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			if (j >= 10)
+			_putchar(j / 10 + '1');
+			_putchar(j % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
