9096a506243fa9c38755ff51e229a6506ebb1514
nathanwanje <nathanwanje76@gmail.com>
2023-09-06 10:36:31 -0700

    libmy.a update

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strstr - The entry point
+ * @haystack: inputs value
+ * @needle: inputs value
+ * Return: Always 0 (Success)
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *l = haystack;
+		char *p = needle;
+
+
+		while (*l == *p && *p != '\0')
+		{
+			l++;
+			p++;
+		}
+
+
+		if (*p == '\0')
+			return (haystack);
+	}
+
+
+	return (0);
+}
